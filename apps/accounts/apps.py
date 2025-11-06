a30651c54cf060ba472d109359e3ba1d52bdd1cb
Andrey Kotlyar <kotlyar562@gmail.com>
2025-11-06 19:16:01 +0200

    feat(accounts): create an app for user's accounts

---
+++ apps/accounts/apps.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025 Andrey Kotlyar
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "accounts"
